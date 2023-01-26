b50a9f0f96b2437594ff12b17f6dae6098321815
Raphael Alampay <raphael.alampay@gmail.com>
2023-01-26 09:47:35 +0800

    added hash map examples

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 using CSharpPatterns.Conf;
 using CSharpPatterns.Services;
 using CSharpPatterns.Interfaces;
+using System.Collections.Generic;
 
 namespace CSharpPatterns
 {
@@ -9,18 +10,44 @@ namespace CSharpPatterns
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Patterns");
+            /*
+            {
+                "firstName": "Raphael",
+                "lastName": "Alampay",
+            }
+            */
+            Dictionary<string, string> person = new Dictionary<string, string>();
+            person.Add("firstName", "Raphael");
+            person.Add("lastName", "Alampay");
 
-            IUserService userService = new MySQLUserService();
+            Console.WriteLine("Full Name: " + person["firstName"] + " " + person["lastName"]);
 
-            List<User> users = GetAllUsers(userService);
-        }
+            /*
+            {
+                "favoriteFood": [
+                    { "name": "Beef" }, { "name": "Chicken Nuggets" }
+                ]
+            }
+            */
+            Dictionary<string, List<Dictionary<string, string>>> favoriteFood = new Dictionary<string, List<Dictionary<string, string>>>();
+            
+            List<Dictionary<string, string>> listOfFood = new List<Dictionary<string, string>>();
+            Dictionary<string, string> beef = new Dictionary<string, string>();
+            beef.Add("name", "Beef");
+            listOfFood.Add(beef);
+            Dictionary<string, string> chicken = new Dictionary<string, string>();
+            chicken.Add("name", "Chicken Nuggets");
+            listOfFood.Add(chicken);
+            
+            favoriteFood.Add(
+                "favoriteFood",
+                listOfFood
+            );
 
-        public static List<User> GetAllUsers(IUserService userService)
-        {
-            List<User> users = userService.GetAll();
+            Console.WriteLine(favoriteFood["favoriteFood"][1]["name"]); // Display Chicken Nuggets
 
-            return users;
+            /*
+            */
         }
     }
 }
